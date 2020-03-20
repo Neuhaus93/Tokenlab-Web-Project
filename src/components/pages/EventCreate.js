de25af81d67a293f75a152a1377d814eccdf3ce4
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2020-03-20 10:30:35 -0300

    Event Create working with the form

--- src/components/pages/EventCreate.js
+++ src/components/pages/EventCreate.js
@@ -1,91 +1,13 @@
 import React from "react";
-import { reset, Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 
 import { createEvent } from "../../actions";
+import EventForm from "./EventForm";
 
 class EventCreate extends React.Component {
-  eventDescription = ({ input }) => (
-    <div className="form-group col-md-6">
-      <label htmlFor="inputCity">Descrição</label>
-      <input
-        {...input}
-        type="text"
-        className="form-control"
-        placeholder="Descrição do evento"
-      />
-    </div>
-  );
-
-  eventCategory = ({ input }) => (
-    <div className="form-group col-md-3">
-      <label htmlFor="inputState">Categoria</label>
-      <select {...input} id="inputState" className="form-control">
-        <option defaultValue="Nada">----</option>
-        <option>Trabalho</option>
-        <option>Lazer</option>
-        <option>Educação</option>
-      </select>
-    </div>
-  );
-
-  eventDate = ({ input }) => (
-    <div className="form-group col-md-3">
-      <label htmlFor="inputEventDate">Data do evento</label>
-      <input
-        {...input}
-        type="date"
-        className="form-control"
-        id="inputEventDate"
-      />
-    </div>
-  );
-
-  eventTimeStart = ({ input }) => (
-    <div className="form-group col-md-2">
-      <label htmlFor="inputStartTime">Hora de Início</label>
-      <input
-        {...input}
-        type="time"
-        className="form-control"
-        id="inputStartTime"
-      />
-    </div>
-  );
-
-  eventTimeEnd = ({ input }) => (
-    <div className="form-group col-md-2">
-      <label htmlFor="inputEndTime">Hora de Término</label>
-      <input
-        {...input}
-        type="time"
-        className="form-control"
-        id="inputEndTime"
-      />
-    </div>
-  );
-
-  isValid = formProps => {
-    const isNotValid =
-      formProps.description == null ||
-      !formProps.description.trim() ||
-      formProps.date == null ||
-      !formProps.date.trim() ||
-      formProps.startTime == null ||
-      !formProps.startTime.trim() ||
-      formProps.endTime == null ||
-      !formProps.endTime.trim();
-    console.log(formProps);
-    return !isNotValid;
-  };
-
-  onSubmit = async (formProps, dispatch) => {
-    if (!this.isValid(formProps)) return;
-    // console.log("Is Valid");
-
-    await dispatch(createEvent(formProps));
+  onSubmit = async formProps => {
+    await this.props.createEvent(formProps);
     this.props.history.push("/events/list");
-    dispatch(reset("signUpForm"));
   };
 
   render() {
@@ -93,28 +15,11 @@ class EventCreate extends React.Component {
       <div className="container">
         <div className="jumbotron">
           <h1 className="display-6 text-center">Criar Evento</h1>
-          <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-            <div className="form-row mt-4 mb-3">
-              <Field name="description" component={this.eventDescription} />
-              <Field name="category" component={this.eventCategory} />
-            </div>
-
-            <div className="form-row mb-3">
-              <Field name="date" component={this.eventDate} />
-              <Field name="startTime" component={this.eventTimeStart} />
-              <Field name="endTime" component={this.eventTimeEnd} />
-            </div>
-
-            <button type="submit" className="btn btn-primary">
-              Cadastre
-            </button>
-          </form>
+          <EventForm onSubmit={this.onSubmit} />
         </div>
       </div>
     );
   }
 }
 
-export default reduxForm({
-  form: "createEventForm"
-})(connect(null, { createEvent })(EventCreate));
+export default connect(null, { createEvent })(EventCreate);
---
+++ src/components/pages/EventForm.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { reset, Field, reduxForm } from "redux-form";
+
+class EventForm extends React.Component {
+  eventDescription = ({ input }) => (
+    <div className="form-group col-md-6">
+      <label htmlFor="inputCity">Descrição</label>
+      <input
+        {...input}
+        type="text"
+        className="form-control"
+        placeholder="Descrição do evento"
+      />
+    </div>
+  );
+
+  eventCategory = ({ input }) => (
+    <div className="form-group col-md-3">
+      <label htmlFor="inputState">Categoria</label>
+      <select {...input} id="inputState" className="form-control">
+        <option defaultValue="Nada">----</option>
+        <option>Trabalho</option>
+        <option>Lazer</option>
+        <option>Educação</option>
+      </select>
+    </div>
+  );
+
+  eventDate = ({ input }) => (
+    <div className="form-group col-md-3">
+      <label htmlFor="inputEventDate">Data do evento</label>
+      <input
+        {...input}
+        type="date"
+        className="form-control"
+        id="inputEventDate"
+      />
+    </div>
+  );
+
+  eventTimeStart = ({ input }) => (
+    <div className="form-group col-md-2">
+      <label htmlFor="inputStartTime">Hora de Início</label>
+      <input
+        {...input}
+        type="time"
+        className="form-control"
+        id="inputStartTime"
+      />
+    </div>
+  );
+
+  eventTimeEnd = ({ input }) => (
+    <div className="form-group col-md-2">
+      <label htmlFor="inputEndTime">Hora de Término</label>
+      <input
+        {...input}
+        type="time"
+        className="form-control"
+        id="inputEndTime"
+      />
+    </div>
+  );
+
+  isValid = formProps => {
+    const isNotValid =
+      formProps.description == null ||
+      !formProps.description.trim() ||
+      formProps.date == null ||
+      !formProps.date.trim() ||
+      formProps.startTime == null ||
+      !formProps.startTime.trim() ||
+      formProps.endTime == null ||
+      !formProps.endTime.trim();
+    return !isNotValid;
+  };
+
+  onSubmit = (formProps, dispatch) => {
+    if (!this.isValid(formProps)) return;
+    // console.log("Is Valid");
+    this.props.onSubmit(formProps);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+        <div className="form-row mt-4 mb-3">
+          <Field name="description" component={this.eventDescription} />
+          <Field name="category" component={this.eventCategory} />
+        </div>
+
+        <div className="form-row mb-3">
+          <Field name="date" component={this.eventDate} />
+          <Field name="startTime" component={this.eventTimeStart} />
+          <Field name="endTime" component={this.eventTimeEnd} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Cadastre
+        </button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "eventForm"
+})(EventForm);
