fd6eda22a335fef5e385835c32b91412bdacc898
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2020-03-24 11:19:36 -0300

    SignInPage fixed!

--- src/components/pages/SignInPage.js
+++ src/components/pages/SignInPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { reset, Field, reduxForm } from "redux-form";
+import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 
 import { logIn, fetchUsers } from "../../actions";
@@ -9,51 +9,64 @@ class SignInPage extends React.Component {
     this.props.fetchUsers();
   }
 
-  renderEmailInput = ({ input }) => {
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div>
+          <em style={{ color: "red", marginLeft: "5px", fontSize: "0.9rem" }}>
+            {error}
+          </em>
+        </div>
+      );
+    }
+  };
+
+  renderEmailInput = ({ input, meta }) => {
     return (
       <div className="form-group">
         <label>Email</label>
         <input
           {...input}
           type="email"
-          className="form-control col-md-4"
+          className="form-control"
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
           placeholder="Insira email"
         />
+        {this.renderError(meta)}
       </div>
     );
   };
 
-  renderPasswordInput = ({ input }) => {
+  renderPasswordInput = ({ input, meta }) => {
     return (
       <div className="form-group">
         <label>Senha</label>
         <input
           {...input}
           type="password"
-          className="form-control col-md-4"
+          className="form-control"
           id="exampleInputPassword1"
           placeholder="Senha"
         />
+        {this.renderError(meta)}
       </div>
     );
   };
 
-  onSubmit = (formProps, dispatch) => {
-    const isSignedIn = () =>
-      new Promise((res, rej) => {
-        dispatch(logIn(formProps));
-        res();
-      });
+  renderNotUser = () => {
+    document.querySelector("#notUser").innerHTML =
+      "O usuário e/ou senha estão incorretos!";
+  };
 
-    isSignedIn().then(() => {
-      if (this.props.isSignedIn) {
-        this.props.history.push("/events/list");
-      } else {
-        dispatch(reset("signInForm"));
-      }
-    });
+  onSubmit = async (formProps, dispatch) => {
+    await dispatch(logIn(formProps));
+
+    if (this.props.isSignedIn) {
+      this.props.history.push("/events/list");
+    } else {
+      this.renderNotUser();
+    }
   };
 
   render() {
@@ -64,6 +77,9 @@ class SignInPage extends React.Component {
           <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
             <Field name="loginEmail" component={this.renderEmailInput} />
             <Field name="loginPassword" component={this.renderPasswordInput} />
+
+            <div id="notUser" className="mb-3"></div>
+
             <button type="submit" className="btn btn-primary">
               Login
             </button>
@@ -74,10 +90,26 @@ class SignInPage extends React.Component {
   }
 }
 
+const validate = formValues => {
+  const { loginEmail, loginPassword } = formValues;
+  const errors = {};
+
+  if (loginEmail == null || !loginEmail.trim()) {
+    errors.loginEmail = "Você deve inserir uma login";
+  }
+
+  if (loginPassword == null || !loginPassword.trim()) {
+    errors.loginPassword = "Você deve inserir uma senha";
+  }
+
+  return errors;
+};
+
 const mapStateToProps = state => ({
   isSignedIn: state.auth.isSignedIn
 });
 
 export default reduxForm({
-  form: "signInForm"
+  form: "signInForm",
+  validate
 })(connect(mapStateToProps, { logIn, fetchUsers })(SignInPage));
